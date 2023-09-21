b9df439e27797fe29d58d0bcb645b1b72ce38d25
Dawa Ometto <d.l.a.ometto1@uu.nl>
2023-09-21 16:29:05 +0200

    Support CUDA 10.2

--- files/cuda_installation.sh
+++ files/cuda_installation.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-  
+CUDA_VERSION="10.2"
+CUDA_DEB_FILE="cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb"
+
 apt update
 apt-get install linux-headers-$(uname -r)
 mkdir /opt/Nvidia-Downloads
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin -O /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda-repo-ubuntu1804-11-1-local_11.1.1-455.32.00-1_amd64.deb -P /opt/Nvidia-Downloads
-dpkg -i /opt/Nvidia-Downloads/cuda-repo-ubuntu1804-11-1-local_11.1.1-455.32.00-1_amd64.deb
-apt-key add /var/cuda-repo-ubuntu1804-11-1-local/7fa2af80.pub
+wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/${CUDA_DEB_FILE} -P /opt/Nvidia-Downloads
+dpkg -i /opt/Nvidia-Downloads/${CUDA_DEB_FILE}
+apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
 apt-get update
 apt-get -y install cuda
-echo "export PATH=/usr/local/cuda-11.1/bin${PATH:+:${PATH}}" > /etc/profile.d/cuda.sh
+echo "export PATH=/usr/local/cuda-10.2/bin${PATH:+:${PATH}}" > /etc/profile.d/cuda.sh
 chmod +x /etc/profile.d/cuda.sh
 /usr/bin/nvidia-persistenced --verbose
 crontab -l | { cat; echo "@reboot /usr/bin/nvidia-persistenced --verbose"; } | crontab -
